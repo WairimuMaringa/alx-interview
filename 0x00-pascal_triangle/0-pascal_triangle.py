d473df5e18cef66558200447a3ba0958a400be25
WairimuMaringa <makena96wairimu@gmail.com>
2023-03-20 11:00:13 -0700

    start

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+Function that returns a list of integers representing
+the Pascals triangle
+"""
+
+
+def pascal_triangle(n):
+    """ Create function that returns list. """
+    if n <= 0:
+        return []
