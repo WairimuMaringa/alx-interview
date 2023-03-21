e60f1c7013c1ea8503cb171730e4a9b9d1ca8a69
WairimuMaringa <makena96wairimu@gmail.com>
2023-03-20 23:48:41 -0700

    taks zero

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -9,3 +9,13 @@ def pascal_triangle(n):
     """ Create function that returns list. """
     if n <= 0:
         return []
+    if n == 1:
+        return [[1]]
+    triangle = [[1]]
+    for i in range(1, n):
+        row = [1]
+        for j in range(1, i):
+            row.append(triangle[i-1][j-1] + triangle[i-1][j])
+        row.append(1)
+        triangle.append(row)
+    return triangle
